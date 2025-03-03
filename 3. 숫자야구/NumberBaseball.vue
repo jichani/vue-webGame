4473090cdf15d0fddcf3ad87648afb756988e4f1
Jichani <123962013+jichani@users.noreply.github.com>
2025-03-03 20:51:14 +0900

    3-4. v-for로 반복문 돌리기

--- 3. 숫자야구/NumberBaseball.vue
+++ 3. 숫자야구/NumberBaseball.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <h1>{{ result }}</h1>
-    <form v-on:submit="onSubmitForm">
-      <input ref="answer" maxlength="4" v-model="value" />
-      <button>입력</button>
+    <form @submit.prevent="onSubmitForm">
+      <input ref="answer" minlength="4" maxlength="4" v-model="value" />
+      <button type="submit">입력</button>
     </form>
-    <div>시도 : {{}}</div>
+    <div>시도 : {{ tries.length }}</div>
+    <ul>
+      <li v-for="(t, index) in tries" v-bind:key="index">
+        <div>{{ t.try }}</div>
+        <div>{{ t.result }}</div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -13,13 +19,19 @@
 export default {
   data() {
     return {
+      tries: [],
       value: "",
       result: "",
     };
   },
-  method: {
-    onSubmitForm(e) {
-      e.preventDefault();
+  methods: {
+    onSubmitForm() {
+      this.tries.push({
+        try: this.value,
+        result: "홈런",
+      });
+      this.value = "";
+      this.$refs.answer.focus();
     },
   },
 };
