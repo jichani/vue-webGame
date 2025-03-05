bd256c18ef3f27b6a9ba352b5a7c0cc7c7ca1c91
Jichani <123962013+jichani@users.noreply.github.com>
2025-03-05 23:00:58 +0900

    4-1. 반응속도체크와 webpack watch

---
+++ 4.반응속도체크/webpack.config.js
@@ -0,0 +1,27 @@
+const { VueLoaderPlugin } = require("vue-loader");
+const path = require("path");
+const { devtools } = require("vue");
+
+module.exports = {
+  mode: "development", // 개발 중이라면 development, 배포용이라면 production
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".vue"],
+  },
+  entry: {
+    app: path.join(__dirname, "main.js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "dist"),
+  },
+};
