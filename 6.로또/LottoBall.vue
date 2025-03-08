f7040fec478bf120d9509a56d5fa5857389a1aae
Jichani <123962013+jichani@users.noreply.github.com>
2025-03-08 18:55:02 +0900

    6-3. 로또 추첨기 구현해보기

--- 6.로또/LottoBall.vue
+++ 6.로또/LottoBall.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ball" :style="{ background }">{{ number }}</div>
+  <div class="ball" :style="styleObject">{{ number }}</div>
 </template>
 
 <script>
@@ -8,10 +8,24 @@ export default {
   props: {
     number: Number,
   },
-  data() {
-    return {
-      background,
-    };
+  computed: {
+    styleObject() {
+      let background;
+      if (this.number <= 10) {
+        background = "red";
+      } else if (this.number <= 20) {
+        background = "orange";
+      } else if (this.number <= 30) {
+        background = "yellow";
+      } else if (this.number <= 40) {
+        background = "green";
+      } else {
+        background = "blue";
+      }
+      return {
+        background,
+      };
+    },
   },
 };
 </script>
--- 6.로또/LottoGenerator.vue
+++ 6.로또/LottoGenerator.vue
@@ -2,11 +2,15 @@
   <div>
     <div>당첨 숫자</div>
     <div id="결과창">
-      <lotto-ball v-for="ball in winBalls" number="10"></lotto-ball>
+      <lotto-ball
+        v-for="ball in winBalls"
+        :key="ball"
+        :number="ball"
+      ></lotto-ball>
     </div>
     <div>보너스</div>
-    <lotto-ball v-if="bonus"></lotto-ball>
-    <button v-if="redo">한 번 더!</button>
+    <lotto-ball v-if="bonus" :number="bonus"></lotto-ball>
+    <button v-if="redo" @click="onClickRedo">한 번 더!</button>
   </div>
 </template>
 
@@ -21,7 +25,7 @@ function getWinNumbers() {
   const shuffle = [];
   while (candidate.length > 0) {
     shuffle.push(
-      candidate.splice(Math.floor(Math.random() * candidates.length), 1)[0]
+      candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
     );
   }
   const bonusNumber = shuffle[shuffle.length - 1];
@@ -29,6 +33,7 @@ function getWinNumbers() {
   return [...winNumbers, bonusNumber];
 }
 
+const timeouts = [];
 export default {
   components: {
     LottoBall,
@@ -42,9 +47,34 @@ export default {
     };
   },
   computed: {},
-  methods: {},
-  mounted() {},
-  beforeDestroy() {},
+  methods: {
+    onClickRedo() {
+      this.winNumbers = getWinNumbers();
+      this.winBalls = [];
+      this.bonus = null;
+      this.redo = false;
+      this.showBalls();
+    },
+    showBalls() {
+      for (let i = 0; i < this.winNumbers.length - 1; i++) {
+        timeouts[i] = setTimeout(() => {
+          this.winBalls.push(this.winNumbers[i]);
+        }, (i + 1) * 1000);
+      }
+      timeouts[6] = setTimeout(() => {
+        this.bonus = this.winNumbers[6];
+        this.redo = true;
+      }, 7000);
+    },
+  },
+  mounted() {
+    this.showBalls();
+  },
+  beforeDestroy() {
+    timeouts.forEach((t) => {
+      clearTimeout(t);
+    });
+  },
   watch: {},
 };
 </script>
